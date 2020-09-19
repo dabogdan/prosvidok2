15b9fb4904f516312a1d8024bb710b5390579619
dabogdan <39666410+dabogdan@users.noreply.github.com>
2020-09-19 19:11:09 +0300

    scroll-up button added

--- js/scroll.js
+++ js/scroll.js
@@ -1,23 +1,34 @@
 // scroll js function
 
-scrollTo = (element) => {
-  window.scroll({
-    behavior: 'smooth',
-    top: element.offsetTop - navbarHeight
-  });
-}
-
 let navbarHeight = document.getElementsByClassName('navbar')[0].clientHeight;
 
-let welcome = document.getElementById('welcome'); 
+// JQuery scroll function
 
-let victims = document.getElementById("victims");
-
-scrollArrow = () => {
-	scrollTo(welcome);
-};
-
-scrollVictims = () => {
-	scrollTo(victims);	
-	mobMenuUncheck();
-}
+$(document).ready(function() {
+// scroll to Top button enable/disable
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 250) {
+            $('.scrollTop').fadeIn(500);
+        } else {
+            $('.scrollTop').fadeOut(500);
+        }
+    });
+// scroll to top func
+    $('.scrollTop').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0}, 300);
+    });
+// scroll to #victims
+    $('.victims-link').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: $('#victims')[0].offsetTop - navbarHeight}, 300);
+    });
+  // scroll to #welcome
+    $('.arrow').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#welcome')[0].offsetTop - navbarHeight}, 300);
+    });
+});
