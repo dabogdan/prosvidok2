40a8b05bbfce6db6254b4c1b98834d1d5db4d42a
dabogdan <39666410+dabogdan@users.noreply.github.com>
2020-10-05 20:22:51 +0300

    courtroom scheme interactive page beta 1

---
+++ js/courtroom.js
@@ -0,0 +1,159 @@
+$('.witness-box').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.bench').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.defence-lawyer').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.defendant-in-custody').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.deliberation-room').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.convoy').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.national-emblem').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.national-flag').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.tv-screen').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.secretary').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.prosecutor').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.right-bar').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.left-bar').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.right-bench').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.left-bench').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
+
+$('.cam').hover(
+    function () {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn();
+    },
+    function () {
+        $('.description').fadeOut(50);
+    }
+)
