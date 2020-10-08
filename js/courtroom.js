e5658c7675978ea794d58e9c95abd19d20ad7029
dabogdan <39666410+dabogdan@users.noreply.github.com>
2020-10-08 11:19:14 +0300

    added courtrooms' pics to the gallery

--- js/courtroom.js
+++ js/courtroom.js
@@ -1,159 +1,159 @@
 $('.witness-box').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.bench').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.defence-lawyer').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.defendant-in-custody').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.deliberation-room').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.convoy').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.national-emblem').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.national-flag').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.tv-screen').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.secretary').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.prosecutor').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.right-bar').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.left-bar').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.right-bench').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.left-bench').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
 
 $('.cam').hover(
     function () {
         $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn();
+        $('.description').fadeIn(25);
     },
     function () {
-        $('.description').fadeOut(50);
+        $('.description').fadeOut(10);
     }
-)
+);
